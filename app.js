c919b6470d180fc663c7f11aa1bb2c7ee0113c1e
mdsoykot <44709658+mdsoykot@users.noreply.github.com>
2019-11-08 11:04:11 +0600

    Add files via upload
    MoGo Full Website

---
+++ app.js
@@ -0,0 +1,53 @@
+
+// start service section
+
+var para=document.querySelector('.text');
+var dClick=document.querySelector('.d-click');
+var uClick=document.querySelector('.u-click');
+
+function dFunction(){
+  para.style.display="inline";
+  dClick.style.display="none";
+  uClick.style.display="inline";
+}
+
+function uFunction(){
+  para.style.display="none";
+  dClick.style.display="inline";
+  uClick.style.display="none";
+}
+
+var para1=document.querySelector('.text1');
+var dClick1=document.querySelector('.d-click1');
+var uClick1=document.querySelector('.u-click1');
+
+function dFunction1(){
+  para1.style.display="inline";
+  dClick1.style.display="none";
+  uClick1.style.display="inline";
+}
+
+function uFunction1(){
+  para1.style.display="none";
+  dClick1.style.display="inline";
+  uClick1.style.display="none";
+}
+
+
+var para2=document.querySelector('.text2');
+var dClick2=document.querySelector('.d-click2');
+var uClick2=document.querySelector('.u-click2');
+
+function dFunction2(){
+  para2.style.display="inline";
+  dClick2.style.display="none";
+  uClick2.style.display="inline";
+}
+
+function uFunction2(){
+  para2.style.display="none";
+  dClick2.style.display="inline";
+  uClick2.style.display="none";
+}
+
+// end service section
